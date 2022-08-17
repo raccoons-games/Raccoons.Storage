70f581754d7b92f1e661819c8612be07eed44576
Vitalii Kryvyi <vitalife@raccoons.games>
2022-08-17 15:21:59 +0300

    RFT-36 Bytes storage

--- Runtime/IStorage.cs
+++ Runtime/IStorage.cs
@@ -17,10 +17,12 @@ namespace Raccoons.Storage
         public Task<string> GetStringAsync(string key, CancellationToken cancellationToken = default);
         public Task<int> GetIntAsync(string key, CancellationToken cancellationToken = default);
         public Task<float> GetFloatAsync(string key, CancellationToken cancellationToken = default);
+        public Task<byte[]> GetBytesAsync(string key, CancellationToken cancellationToken = default);
 
         public Task SetStringAsync(string key, string value, CancellationToken cancellationToken = default);
         public Task SetIntAsync(string key, int value, CancellationToken cancellationToken = default);
         public Task SetFloatAsync(string key, float value, CancellationToken cancellationToken = default);
+        public Task SetBytesAsync(string key, byte[] value, CancellationToken cancellationToken = default);
 
         public Task<bool> ExistsAsync(string key, CancellationToken cancellationToken = default);
         public Task DeleteAsync(string key, CancellationToken cancellationToken = default);
@@ -28,10 +30,12 @@ namespace Raccoons.Storage
         public string GetString(string key);
         public int GetInt(string key);
         public float GetFloat(string key);
+        public byte[] GetBytes(string key);
 
         public void SetString(string key, string value);
         public void SetFloat(string key, float value);
         public void SetInt(string key, int value);
+        public void SetBytes(string key, byte[] value);
 
         public bool Exists(string key);
         public void Delete(string key);
