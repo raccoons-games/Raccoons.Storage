6685fb3980bea0d635f4830c9f3f1af26dfcd4e9
thevitalife <37376394+thevitalife@users.noreply.github.com>
2022-08-17 17:08:46 +0300

    RFT-37 storage assets (#5)
    * RFT-37 Created scriptable assets for storages
    
    Co-authored-by: Vitalii Kryvyi <vitalife@raccoons.games>

---
+++ Runtime/Instances/PlayerPrefsStorageAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Raccoons.Storage.Instances
+{
+    [CreateAssetMenu(fileName = "PlayerPrefsStorageAsset", menuName = "Raccoons/Storage/PlayerPrefsStorageAsset")]
+    public class PlayerPrefsStorageAsset : BaseStorageAsset
+    {
+        protected override IStorage CreateStorage(string key, IStorage parent)
+        {
+            return new PlayerPrefsStorage(key, parent);
+        }
+    }
+}
