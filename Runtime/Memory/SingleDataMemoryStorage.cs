d281fb3b6121ecd8f2daad7c2ed1fb9b864ff3a1
Vitalii Kryvyi <vitalife@raccoons.games>
2022-11-27 23:07:49 +0200

    RFT-55 Added SingleDataMemoryStorage

---
+++ Runtime/Memory/SingleDataMemoryStorage.cs
@@ -0,0 +1,159 @@
+﻿using Raccoons.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Raccoons.Storages.Memory
+{
+    public class SingleDataMemoryStorageChannel : IStorageChannel
+    {
+        private object _data = default;
+
+        public SingleDataMemoryStorageChannel(object initialData, string initialKey)
+        {
+            _data = initialData;
+            Key = initialKey;
+        }
+
+        public SingleDataMemoryStorageChannel() : this(default, default)
+        {
+
+        }
+
+        public string Key { get; private set; }
+
+        public void Delete(string key)
+        {
+            if (key.Equals(Key))
+            {
+                _data = default;
+                Key = default;
+            }
+        }
+
+        public Task DeleteAsync(string key, CancellationToken cancellationToken = default)
+        {
+            Delete(key);
+            return Task.CompletedTask;
+        }
+
+        public bool Exists(string key)
+        {
+            return (Key.Equals(key));
+        }
+
+        public Task<bool> ExistsAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Exists(key));
+        }
+
+        public TData GetData<TData>(string key)
+        {
+            if (Key.Equals(key))
+            {
+                return (TData)_data;
+            }
+            return default;
+        }
+
+        public Task<TData> GetDataAsync<TData>(string key, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(GetData<TData>(key));
+        }
+
+        public byte[] GetBytes(string key)
+        {
+            return GetData<byte[]>(key);
+        }
+
+        public Task<byte[]> GetBytesAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return GetDataAsync<byte[]>(key, cancellationToken);
+        }
+
+        public float GetFloat(string key)
+        {
+            return GetData<float>(key);
+        }
+
+        public Task<float> GetFloatAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return GetDataAsync<float>(key, cancellationToken);
+        }
+
+        public int GetInt(string key)
+        {
+            return GetData<int>(key);
+        }
+
+        public Task<int> GetIntAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return GetDataAsync<int>(key, cancellationToken);
+        }
+
+        public string GetString(string key)
+        {
+            return GetData<string>(key);
+        }
+
+        public Task<string> GetStringAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return GetDataAsync<string>(key, cancellationToken);
+        }
+
+        public void SetData(string key, object data)
+        {
+            Key = key;
+            _data = data;
+        }
+
+        public Task SetDataAsync(string key, object data)
+        {
+            SetData(key, data);
+            return Task.CompletedTask;
+        }
+
+        public void SetBytes(string key, byte[] value)
+        {
+            SetData(key, value);
+        }
+
+        public Task SetBytesAsync(string key, byte[] value, CancellationToken cancellationToken = default)
+        {
+            return SetDataAsync(key, value);
+        }
+
+        public void SetFloat(string key, float value)
+        {
+            SetData(key, value);
+        }
+
+        public Task SetFloatAsync(string key, float value, CancellationToken cancellationToken = default)
+        {
+            return SetDataAsync(key, value);
+        }
+
+        public void SetInt(string key, int value)
+        {
+            SetData(key, value);
+        }
+
+        public Task SetIntAsync(string key, int value, CancellationToken cancellationToken = default)
+        {
+            return SetDataAsync(key, value);
+        }
+
+        public void SetString(string key, string value)
+        {
+            SetData(key, value);
+        }
+
+        public Task SetStringAsync(string key, string value, CancellationToken cancellationToken = default)
+        {
+            return SetDataAsync(key, value);
+        }
+    }
+}
