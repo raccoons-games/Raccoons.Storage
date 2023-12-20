048e442f23258a95a2066a554c82f38947a85455
Vitalii Kryvyi <vitalife@raccoons.games>
2023-12-20 11:08:28 +0200

    RFT-000 Quick fix for editor assembly

--- Editor/Cryptography/Aes/AesEncryptionAssetEditor.cs
+++ Editor/Cryptography/Aes/AesEncryptionAssetEditor.cs
@@ -1,24 +1,22 @@
+using Raccoons.Storage.Cryptography.Aes;
 using UnityEditor;
 using UnityEngine;
 
-namespace Raccoons.Storage.Cryptography.Aes
-{
-    [CustomEditor(typeof(AesEncryptionAsset))]
 
-    public class AesEncryptionAssetEditor : Editor
+[CustomEditor(typeof(AesEncryptionAsset))]
+public class AesEncryptionAssetEditor : Editor
+{
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
+        base.OnInspectorGUI();
+        var script = (AesEncryptionAsset)target;
+        if (GUILayout.Button("Verify", GUILayout.Height(40)))
         {
-            base.OnInspectorGUI();
-            var script = (AesEncryptionAsset)target;
-            if (GUILayout.Button("Verify", GUILayout.Height(40)))
-            {
-                script.Verify();
-            }
-            if (GUILayout.Button("Regenerate", GUILayout.Height(40)))
-            {
-                script.Regenerate();
-            }
+            script.Verify();
+        }
+        if (GUILayout.Button("Regenerate", GUILayout.Height(40)))
+        {
+            script.Regenerate();
         }
     }
 }
